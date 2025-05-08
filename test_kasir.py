2fd4c4cb71a46413e2a0e9edf94a3c7276b35ee9
maulidyafatima <marsyaa307@gmail.com>
2025-05-08 15:46:18 +0700

    Maulidya - Memperbarui Code

--- test_kasir.py
+++ test_kasir.py
@@ -4,20 +4,20 @@ from kasir import GenericList, ItemBelanja
 class TestKasir(unittest.TestCase):
     def test_tambah_item(self):
         kasir = GenericList[ItemBelanja]()
-        item = ItemBelanja("Batik", 100000, 2)
+        item = ItemBelanja("Indomie", 3000, 2)
         kasir.add_item(item)
         self.assertEqual(kasir.count(), 1)
 
     def test_subtotal(self):
-        item = ItemBelanja("Kemeja", 75000, 3)
-        self.assertEqual(item.subtotal(), 225000)
+        item = ItemBelanja("Aqua", 5000, 3)
+        self.assertEqual(item.subtotal(), 15000)
 
     def test_total_harga_dan_diskon(self):
         kasir = GenericList[ItemBelanja]()
-        kasir.add_item(ItemBelanja("Sepatu", 200000, 1))
-        kasir.add_item(ItemBelanja("Kaos", 100000, 2))
-        self.assertEqual(kasir.total_harga(), 400000)
-        self.assertAlmostEqual(kasir.total_setelah_diskon(0.1), 360000)
+        kasir.add_item(ItemBelanja("Teh Botol", 7000, 2))  # 14.000
+        kasir.add_item(ItemBelanja("Roti Tawar", 12000, 1))  # 12.000
+        self.assertEqual(kasir.total_harga(), 26000)
+        self.assertAlmostEqual(kasir.total_setelah_diskon(0.1), 23400)  # diskon 10%
 
 if __name__ == '__main__':
     unittest.main()
